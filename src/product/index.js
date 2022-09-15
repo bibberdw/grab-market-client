28799d6c6c0265731fcca86a6353ed139d84152f
DONG WOO <DONGWOO@LEEui-MacBookPro.local>
2022-09-16 00:19:43 +0900

    antd 기본 적용

--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,45 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function ProductPage() {
-  const params = useParams();
-  console.log(params);
-  return <h1>상품 상세 페이지</h1>;
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(function () {
+    axios
+      .get(
+        `https://ef149467-ea95-4da8-a3f0-45d954a134b4.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
+  console.log(product);
+  if (product === null) {
+    return <h1>상품 정보를 받고 있습니다...</h1>;
+  }
+
+  return (
+    <div>
+      <div id="image-box">
+        <img src={"/" + product.imageUrl} />
+      </div>
+      <div id="profile-box">
+        <img src="/images/icons/avatar.png" />
+        <span>{product.seller}</span>
+      </div>
+      <div id="content-box">
+        <div id="name">{product.name}</div>
+        <div id="price">{product.price}원</div>
+        <div id="createdAt">2020년 12월 8일</div>
+        <div id="description">{product.description}</div>
+      </div>
+    </div>
+  );
 }
 export default ProductPage;
