034326ce26672533b14598b68ccbc6fafa48cd5c
DONG WOO <DONGWOO@LEEui-MacBookPro.local>
2022-09-17 23:15:37 +0900

    결제전까지 구현

--- src/product/index.js
+++ src/product/index.js
@@ -2,24 +2,23 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./index.css";
+import { API_URL } from "../config/constants";
+import dayjs from "dayjs";
 
 function ProductPage() {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
-
   useEffect(function () {
     axios
-      .get(
-        `https://ef149467-ea95-4da8-a3f0-45d954a134b4.mock.pstmn.io/products/${id}`
-      )
+      .get(`${API_URL}/products/${id}`)
       .then(function (result) {
-        setProduct(result.data);
+        setProduct(result.data.product);
       })
       .catch(function (error) {
         console.error(error);
       });
   }, []);
-  console.log(product);
+
   if (product === null) {
     return <h1>상품 정보를 받고 있습니다...</h1>;
   }
@@ -27,19 +26,22 @@ function ProductPage() {
   return (
     <div>
       <div id="image-box">
-        <img src={"/" + product.imageUrl} />
+        <img src={`${API_URL}/${product.imageUrl}`} />
       </div>
       <div id="profile-box">
         <img src="/images/icons/avatar.png" />
         <span>{product.seller}</span>
       </div>
-      <div id="content-box">
+      <div id="contents-box">
         <div id="name">{product.name}</div>
         <div id="price">{product.price}원</div>
-        <div id="createdAt">2020년 12월 8일</div>
-        <div id="description">{product.description}</div>
+        <div id="createdAt">
+          {dayjs(product.createdAt).format("YYYY년 MM월 DD일")}
+        </div>
+        <pre id="description">{product.description} </pre>
       </div>
     </div>
   );
 }
+
 export default ProductPage;
